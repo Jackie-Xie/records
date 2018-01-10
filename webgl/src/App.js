6f643619473b3f53f039e34b0d6990dfdc43eaa1
mc-tangb <511598230@qq.com>
2018-01-10 19:28:14 +0800

    简单的3d例子

--- webgl/src/App.js
+++ webgl/src/App.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
-import Demo1 from './container/demo1'
+import Demo1 from './container/demo1';
+import Detector from './utils/detector'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <Demo1 />
+       { Detector.webgl ? <Demo1 /> : <Warning />}
       </div>
     );
+  }
+}
+
+class Warning extends Component {
+  dom = ''
+  componentDidMount() {
+    var warning = Detector.getWebGLErrorMessage();
+    this.dom.appendChild(warning);
+  }
+  
+  render() {
+    return (
+      <div ref={(dom) => this.dom = dom}>
+      
+      </div>
+    )
   }
 }
 
--- webgl/src/container/demo1.js
+++ webgl/src/container/demo1.js
@@ -9,13 +9,76 @@ class Demo1 extends Component {
     dom = ''
 
     componentDidMount() {
-        let { width, height } = this.dom.style;
-        let scene = new Three.Scene();
-        let camera = new Three.PerspectiveCamera(45, this.dom.style.width, this.dom.style.height, 0.1, 1000);
-        let renderer = new Three.WebGLRenderer();
+        //this.demo1();
+        this.drawingLines();
+    }
 
-        renderer.setClearColorHex(0xEEEEEE);
+    /** 
+     * @desc quick start 
+    */
+    demo1() {
+        let {
+            width,
+            height
+        } = getComputedStyle(this.dom);
+        width = +width.slice(0, -2);
+        height = +height.slice(0, -2);
+        let scene = new Three.Scene();
+        let camera = new Three.PerspectiveCamera(45, width / height, 0.1, 1000);
+        let renderer = new Three.WebGLRenderer();
         renderer.setSize(width, height);
+        this.dom.appendChild(renderer.domElement);
+
+        let geometry = new Three.BoxGeometry(1, 1, 1);
+        let material = new Three.MeshBasicMaterial({
+            color: 0x00ff00
+        });
+
+        let cube = new Three.Mesh(geometry, material);
+        scene.add(cube);
+
+        camera.position.z = 5;
+
+        let animate = () => {
+            requestAnimationFrame(animate);
+
+            //cube.rotation.x += 0.04;
+            cube.rotation.y += 0.04;
+            cube.rotation.z += 0.04;
+
+            renderer.render(scene, camera);
+        }
+
+        animate();
+    }
+
+    drawingLines() {
+        let {
+            width,
+            height
+        } = getComputedStyle(this.dom);
+        width = +width.slice(0, -2);
+        height = +height.slice(0, -2);
+        let scene = new Three.Scene();
+        let camera = new Three.PerspectiveCamera(45, width / height, 0.1, 1000);
+        let renderer = new Three.WebGLRenderer();
+        renderer.setSize(width, height);
+        this.dom.appendChild(renderer.domElement);
+
+        camera.position.set(0, 0, 50);
+        camera.lookAt(new Three.Vector3(0, 0, 0));
+
+        let material = new Three.LineBasicMaterial({color: 0x0000ff});
+
+        let geometry = new Three.Geometry();
+        geometry.vertices.push(new Three.Vector3(-10, 0, 0));
+        geometry.vertices.push(new Three.Vector3(0, 10, 0));
+        geometry.vertices.push(new Three.Vector3(10, 0, 0));
+
+        let line = new Three.Line(geometry, material);
+
+        scene.add(line);
+        renderer.render(scene, camera);
     }
 
     render() {
