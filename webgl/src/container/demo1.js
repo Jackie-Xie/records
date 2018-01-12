2f4b5f53a35f631d532851ff4a728dc8e98e678b
mc-tangb <511598230@qq.com>
2018-01-12 10:51:44 +0800

    平面例子

--- webgl/src/container/demo1.js
+++ webgl/src/container/demo1.js
@@ -10,7 +10,8 @@ class Demo1 extends Component {
 
     componentDidMount() {
         //this.demo1();
-        this.drawingLines();
+        //this.drawingLines();
+        this.drawPlane();
     }
 
     /** 
@@ -79,6 +80,55 @@ class Demo1 extends Component {
 
         scene.add(line);
         renderer.render(scene, camera);
+    }
+
+    drawPlane() {
+        let {
+            width,
+            height
+        } = getComputedStyle(this.dom);
+        width = +width.slice(0, -2);
+        height = +height.slice(0, -2);
+
+        let scene = new Three.Scene();
+
+        let camera = new Three.PerspectiveCamera(45, width / height, 0.1, 1000);
+        scene.add(camera);
+
+        let renderer = new Three.WebGLRenderer();
+        renderer.setClearColor(0xeeeeee, 1.0);
+        renderer.setSize(width, height);
+        renderer.shadowMapEnabled = true;
+        this.dom.appendChild(renderer.domElement);
+
+        let planeGeometry = new Three.PlaneGeometry(30, 20, 1, 1);
+        let planeMaterial = new Three.MeshLambertMaterial({color: 0xffffff});
+        let plane = new Three.Mesh(planeGeometry, planeMaterial);
+        plane.receiveShadow = true;
+
+        plane.rotation.x = -0.5 * Math.PI;
+        plane.position.x = 0;
+        plane.position.y = 0;
+        plane.position.z = 0;
+
+        scene.add(plane);
+
+        camera.position.x = -30;
+        camera.position.y = 40;
+        camera.position.z = 30;
+        camera.lookAt(scene.position);
+
+        let ambientLight = new Three.AmbientLight(0x0c0c0c);
+        scene.add(ambientLight);
+
+        let spotLight = new Three.SpotLight(0xffffff);
+        spotLight.position.set(-40, 60, -10);
+        spotLight.castShadow = true;
+        scene.add(spotLight);
+
+        renderer.render(scene, camera);
+
+
     }
 
     render() {
